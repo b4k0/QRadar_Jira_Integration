9dd4fd02607f431e5bd97ad2a7b2c87aa6cfb04e
Mpak1996 <giannismpak@outlook.com>
2023-12-19 12:15:08 +0200

    Upload Test Script for QRadar API

---
+++ TestQradarApi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import requests
+import sys
+import urllib3
+
+# Disable warning
+urllib3.disable_warnings()
+
+# Variables for QRadar API Request
+apiToken= sys.argv[1] 
+endpointURL = sys.argv[2]
+offenseID = sys.argv[3]
+
+# Endpoint of QRadar API Request
+endpoint = "https://{0}/api/siem/offenses?fields=id%2C%20description%2C%20magnitude%2C%20destination_networks&filter=id%20%3D%20'{1}'".format(endpointURL, offenseID)
+
+# Header of QRadar API Request using QRadar API Token
+header = {'Accept': 'application/json', 'Connection': 'keep-alive', 'SEC': apiToken}
+
+# GET Request for retrieving QRadar Offenses
+request = requests.get(endpoint, headers=header, verify=False, timeout=60)
+
+# Print Response
+print(request.json())
